a42da1e928925567b9835006fc12307fb2a1a4f9
amit mishra <amit.m@eclateng.com>
2023-09-06 14:37:58 +0530

    PWA home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,28 @@ module.exports = {
   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {
+      colors:{
+        // background color
+        'pwa-orange': "#FF0420",
+        'pwa-orange-light': "#FF5754",
+        'pwa-yellow-bg-50': "#FFE6B1",
+        'pwa-yellow-bg-100': "#F6C25F",
+         // text color
+         'pwa-gray-text-100': "#181818",
+      },
+      boxShadow:{
+        'paw-sm': '0px 3px 10px 0px rgba(0, 0, 0, 0.25)',
+        'paw-lg': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+      },
       screens: {
         '3xl': '1600px',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('postcss-import'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
